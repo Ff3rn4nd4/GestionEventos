2a0f93d4a3e438f9f466ab6cbbb5f56cb92e47b6
Fernanda Guerra <guerrareyes_fernanda@hotmail.com>
2023-05-16 01:40:04 -0600

    Registro Asistencia arreglar BD

---
+++ GestionEventos/Migrations/20230516073039_RegistroAsistencias.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionEventos.Migrations
+{
+    /// <inheritdoc />
+    public partial class RegistroAsistencias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
